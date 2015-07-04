b1aa16484b5a9cb7ee77eda22ebbfbda914a0923
dipuhasan <depu1994@gmail.com>
2015-07-04 09:35:49 +0600

    1585 Making kites.cpp

---
+++ 1585 Making kites.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a, b, T;
+    scanf("%d", &T);
+    while(T--){
+        cin>>a>>b;
+        cout<<(a*b)/2<<" cm2"<<endl;
+    }
+
+    return 0;
+}
