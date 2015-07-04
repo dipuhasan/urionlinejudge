454156b7f4c829e74ed09c1eead41e954246e8e6
dipuhasan <depu1994@gmail.com>
2015-07-04 09:35:29 +0600

    1546. Feedback.cpp

---
+++ 1546. Feedback.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int T, z, name;
+    scanf("%d", &T);
+    while(T--){
+        scanf("%d", &z);
+        for(int i = 0; i<z; i++){
+            scanf("%d", &name);
+        if(name == 1)
+            printf("Rolien\n");
+        if(name == 2)
+            printf("Naej\n");
+        if(name == 3)
+            printf("Elehcim\n");
+        if(name == 4)
+            printf("Odranoel\n");
+        }
+
+    }
+
+
+    return 0;
+}
