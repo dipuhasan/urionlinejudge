11937cbb32918d763fed4122355493ba1d0f9a2e
dipuhasan <depu1994@gmail.com>
2015-07-04 09:35:04 +0600

    1387.Og.cpp

---
+++ 1387.Og.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int m, n;
+    while(scanf("%d %d", &m, &n)==2){
+        if(m==0 && n==0) break;
+        printf("%d\n", m+n);
+    }
+
+
+    return 0;
+}
